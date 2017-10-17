9e63f183deb524f32dbee9a33a9561a1b48c145a
Shin So <sso@yahoo-corp.jp>
2017-10-17 15:10:45 +0900

    Add failure info / split stats test case

--- src/test/java/jp/co/yahoo/presto/audit/TestAuditLogListener.java
+++ src/test/java/jp/co/yahoo/presto/audit/TestAuditLogListener.java
@@ -13,8 +13,11 @@
  */
 package jp.co.yahoo.presto.audit;
 
+import com.facebook.presto.spi.ErrorCode;
+import com.facebook.presto.spi.ErrorType;
 import com.facebook.presto.spi.eventlistener.QueryCompletedEvent;
 import com.facebook.presto.spi.eventlistener.QueryContext;
+import com.facebook.presto.spi.eventlistener.QueryFailureInfo;
 import com.facebook.presto.spi.eventlistener.QueryIOMetadata;
 import com.facebook.presto.spi.eventlistener.QueryInputMetadata;
 import com.facebook.presto.spi.eventlistener.QueryMetadata;
@@ -93,7 +96,35 @@ public class TestAuditLogListener
         AuditRecord record = listener.buildAuditRecord(queryCompletedEvent);
         Gson obj = new GsonBuilder().disableHtmlEscaping().create();
 
-        String expected = "{\"queryId\":\"20170606_044544_00024_nfhe3\",\"query\":\"select * from airdelays_s3_csv WHERE kw = 'presto-kw-example' limit 5\",\"uri\":\"http://example.com:8080/v1/query/20170521_140224_00002_gd5k3\",\"state\":\"FINISHED\",\"cpuTime\":0.1,\"wallTime\":0.2,\"queuedTime\":0.3,\"peakMemoryBytes\":10001,\"totalBytes\":10002,\"totalRows\":10003,\"createTime\":\"20170715100000.000\",\"executionStartTime\":\"20170715100001.000\",\"endTime\":\"20170715100003.000\",\"remoteClientAddress\":\"example.com\",\"clientUser\":\"test-user\",\"userAgent\":\"StatementClient 0.167\",\"source\":\"presto-cli\"}";
-        assertThat(obj.toJson(record)).isEqualTo(expected);
+        // Normal
+        String expected_normal = "{\"queryId\":\"20170606_044544_00024_nfhe3\",\"query\":\"select * from airdelays_s3_csv WHERE kw = 'presto-kw-example' limit 5\",\"uri\":\"http://example.com:8080/v1/query/20170521_140224_00002_gd5k3\",\"state\":\"FINISHED\",\"cpuTime\":0.1,\"wallTime\":0.2,\"queuedTime\":0.3,\"peakMemoryBytes\":10001,\"totalBytes\":10002,\"totalRows\":10003,\"completedSplits\":14,\"createTime\":\"20170715100000.000\",\"executionStartTime\":\"20170715100001.000\",\"endTime\":\"20170715100003.000\",\"remoteClientAddress\":\"example.com\",\"clientUser\":\"test-user\",\"userAgent\":\"StatementClient 0.167\",\"source\":\"presto-cli\"}";
+        assertThat(obj.toJson(record)).isEqualTo(expected_normal);
+
+        // Failure
+        metadata = new QueryMetadata("20170606_044544_00024_nfhe3",
+                Optional.of("4c52973c-14c6-4534-837f-238e21d9b061"),
+                "select 2a",
+                "FAILED",
+                uri,
+                Optional.empty());
+        ErrorCode errorCode = new ErrorCode(1, "SYNTAX_ERROR", ErrorType.USER_ERROR);
+        QueryFailureInfo failureInfo = new QueryFailureInfo(errorCode,
+                Optional.of("com.facebook.presto.sql.parser.ParsingException"),
+                Optional.of("line 1:15: mismatched input '0' expecting ')'"),
+                Optional.empty(),
+                Optional.empty(),
+                "{json-error}"
+        );
+        queryCompletedEvent = new QueryCompletedEvent(metadata,
+                statistics,
+                context,
+                ioMetadata,
+                Optional.of(failureInfo),
+                createTime,
+                executionStartTime,
+                endTime);
+        record = listener.buildAuditRecord(queryCompletedEvent);
+        String expected_failure = "{\"queryId\":\"20170606_044544_00024_nfhe3\",\"query\":\"select 2a\",\"uri\":\"http://example.com:8080/v1/query/20170521_140224_00002_gd5k3\",\"state\":\"FAILED\",\"cpuTime\":0.1,\"wallTime\":0.2,\"queuedTime\":0.3,\"peakMemoryBytes\":10001,\"totalBytes\":10002,\"totalRows\":10003,\"completedSplits\":14,\"createTime\":\"20170715100000.000\",\"executionStartTime\":\"20170715100001.000\",\"endTime\":\"20170715100003.000\",\"errorCode\":\"SYNTAX_ERROR:1\",\"failureType\":\"com.facebook.presto.sql.parser.ParsingException\",\"failureMessage\":\"line 1:15: mismatched input '0' expecting ')'\",\"failuresJson\":\"{json-error}\",\"remoteClientAddress\":\"example.com\",\"clientUser\":\"test-user\",\"userAgent\":\"StatementClient 0.167\",\"source\":\"presto-cli\"}";
+        assertThat(obj.toJson(record)).isEqualTo(expected_failure);
     }
 }
