cbaac5d0c251f2b3242113d76c02becea66a664e
Star Poon <ypoon@yahoo-corp.jp>
2018-06-14 09:41:38 +0900

    Add test case for PulsarProducer

--- src/main/java/jp/co/yahoo/presto/audit/pulsar/PulsarProducer.java
+++ src/main/java/jp/co/yahoo/presto/audit/pulsar/PulsarProducer.java
@@ -13,6 +13,7 @@
  */
 package jp.co.yahoo.presto.audit.pulsar;
 
+import com.google.common.annotations.VisibleForTesting;
 import io.airlift.log.Logger;
 import jp.co.yahoo.presto.audit.serializer.SerializedLog;
 import org.apache.pulsar.client.api.ClientConfiguration;
@@ -28,22 +29,12 @@ import java.util.concurrent.TimeUnit;
 public class PulsarProducer
 {
     private static final Logger log = Logger.get(PulsarProducer.class);
-    private PulsarClient pulsarClient;
     private Producer producer;
 
-    public void initProducer(String topic, String url, String trustCerts, Map<String, String> authParams, boolean useTLS)
-            throws PulsarClientException
+    @VisibleForTesting
+    public PulsarProducer(Producer producer)
     {
-        ClientConfiguration conf = new ClientConfiguration();
-        conf.setUseTls(useTLS);
-        conf.setTlsTrustCertsFilePath(trustCerts);
-        conf.setAuthentication(AuthenticationAthenz.class.getName(), authParams);
-
-        pulsarClient = PulsarClient.create(url, conf);
-
-        ProducerConfiguration prodConf = new ProducerConfiguration();
-        prodConf.setSendTimeout(3, TimeUnit.SECONDS);
-        producer = pulsarClient.createProducer(topic, prodConf);
+        this.producer = producer;
     }
 
     public void send(SerializedLog message)
@@ -52,14 +43,84 @@ public class PulsarProducer
             producer.send(message.getSerializedLog().getBytes());
         }
         catch (PulsarClientException e) {
-            log.error("Failed to send message to Pulsar broker.", e);
+            log.error("Failed to send message to Pulsar broker. " + e);
             log.error("Dropped queryID: " + message.getQueryId());
         }
     }
 
-    public void close()
-            throws PulsarClientException
+    public static class Builder
     {
-        pulsarClient.close();
+        private String topic;
+        private String url;
+        private String trustCerts;
+        private Map<String, String> authParams;
+        private boolean useTLS;
+        private int sendTimeout = 3;
+        private TimeUnit sendTimeoutUnit = TimeUnit.SECONDS;
+
+        public Builder setTopic(String topic)
+        {
+            this.topic = topic;
+            return this;
+        }
+
+        public Builder setURL(String url)
+        {
+            this.url = url;
+            return this;
+        }
+
+        public Builder setTrustCerts(String trustCerts)
+        {
+            this.trustCerts = trustCerts;
+            return this;
+        }
+
+        public Builder setAuthParams(Map<String, String> authParams)
+        {
+            this.authParams = authParams;
+            return this;
+        }
+
+        public Builder setUseTLS(boolean useTLS)
+        {
+            this.useTLS = useTLS;
+            return this;
+        }
+
+        public Builder setSendTimeout(int sendTimeout, TimeUnit timeUnit)
+        {
+            this.sendTimeout = sendTimeout;
+            this.sendTimeoutUnit = timeUnit;
+            return this;
+        }
+
+        @VisibleForTesting
+        ClientConfiguration buildClientConfiguration(ClientConfiguration conf)
+                throws PulsarClientException.UnsupportedAuthenticationException
+        {
+            // Config PulsarClient
+            conf.setUseTls(useTLS);
+            conf.setTlsTrustCertsFilePath(trustCerts);
+            conf.setAuthentication(AuthenticationAthenz.class.getName(), authParams);
+            return conf;
+        }
+
+        @VisibleForTesting
+        ProducerConfiguration buildProducerConfiguration(ProducerConfiguration prodConf)
+        {
+            prodConf.setSendTimeout(sendTimeout, sendTimeoutUnit);
+            return prodConf;
+        }
+
+        public PulsarProducer build()
+                throws PulsarClientException
+        {
+            ClientConfiguration conf = buildClientConfiguration(new ClientConfiguration());
+            ProducerConfiguration prodConf = buildProducerConfiguration(new ProducerConfiguration());
+            PulsarClient pulsarClient = PulsarClient.create(url, conf);
+            Producer producer = pulsarClient.createProducer(topic, prodConf);
+            return new PulsarProducer(producer);
+        }
     }
 }
---
+++ src/test/java/jp/co/yahoo/presto/audit/pulsar/TestPulsarProducer.java
@@ -0,0 +1,90 @@
+package jp.co.yahoo.presto.audit.pulsar;
+
+import jp.co.yahoo.presto.audit.serializer.SerializedLog;
+import org.apache.pulsar.client.api.ClientConfiguration;
+import org.apache.pulsar.client.api.Producer;
+import org.apache.pulsar.client.api.ProducerConfiguration;
+import org.apache.pulsar.client.api.PulsarClientException;
+import org.apache.pulsar.client.impl.auth.AuthenticationAthenz;
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class TestPulsarProducer
+{
+    @Test
+    public void TestClientConfigurationBuilder()
+            throws PulsarClientException.UnsupportedAuthenticationException
+    {
+        Map<String, String> authParams = new HashMap<>();
+        ClientConfiguration mockConf = mock(ClientConfiguration.class);
+        ClientConfiguration conf = new PulsarProducer.Builder()
+                .setUseTLS(false)
+                .setTrustCerts("/path/to/trusted_cert")
+                .setAuthParams(authParams)
+                .buildClientConfiguration(mockConf);
+
+        assert(mockConf == conf);
+        verify(mockConf).setUseTls(false);
+        verify(mockConf).setTlsTrustCertsFilePath("/path/to/trusted_cert");
+        verify(mockConf).setAuthentication(AuthenticationAthenz.class.getName(), authParams);
+    }
+
+    @Test
+    public void TestProducerConfigurationBuilderDefault()
+    {
+        ProducerConfiguration mockConf = mock(ProducerConfiguration.class);
+        ProducerConfiguration conf = new PulsarProducer.Builder()
+                .buildProducerConfiguration(mockConf);
+
+        assert(mockConf == conf);
+        verify(mockConf).setSendTimeout(3, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void TestProducerConfigurationBuilderConfig()
+    {
+        ProducerConfiguration mockConf = mock(ProducerConfiguration.class);
+        ProducerConfiguration conf = new PulsarProducer.Builder()
+                .setSendTimeout(30, TimeUnit.MILLISECONDS)
+                .buildProducerConfiguration(mockConf);
+
+        assert(mockConf == conf);
+        verify(mockConf).setSendTimeout(30, TimeUnit.MILLISECONDS);
+    }
+
+    @Test(expectedExceptions = PulsarClientException.class)
+    public void TestBuilderFail()
+            throws PulsarClientException
+    {
+        new PulsarProducer.Builder()
+                .setURL("pulsar+ssl://pulsar.cluster.com:6651")
+                .setTopic("persistent://namespace/global/test/topic1").build();
+    }
+
+    @Test
+    public void TestPulsarProducer()
+    {
+        Producer producer = mock(Producer.class);
+        PulsarProducer pulsarProducer = new PulsarProducer(producer);
+        pulsarProducer.send(new SerializedLog("queryID", "{\"a\":\"b\"}"));
+    }
+
+    @Test
+    public void TestPulsarProducerSendError()
+            throws PulsarClientException
+    {
+        Producer producer = mock(Producer.class);
+        when(producer.send(any(byte[].class)))
+                .thenThrow(new PulsarClientException("Mock IO Exception"));
+        PulsarProducer pulsarProducer = new PulsarProducer(producer);
+        pulsarProducer.send(new SerializedLog("being_dropped_queryID", "{\"a\":\"b\"}"));
+    }
+}
